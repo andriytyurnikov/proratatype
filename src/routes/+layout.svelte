971f573b922f18523e5065f9a53c291cf3674e49
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2023-12-05 05:27:10 +0200

    shadcn-svelte added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,18 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script lang="ts">
+  import "../app.pcss";
+  
+  import { ModeWatcher } from "mode-watcher";
+</script>
+
+<ModeWatcher />
+
+<div class="container">
+  <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    Layout
+  </h1>
+  <p class="text-xl text-muted-foreground">
+    Common UI element between pages
+  </p>
+</div>
+
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <p class="leading-7 [&:not(:first-child)]:mt-6">
+    The king, seeing how much happier his subjects were, realized the error of his
+    ways and repealed the joke tax.
+  </p>
+</div>
