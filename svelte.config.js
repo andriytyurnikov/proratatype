3dca0ffcf7ed8ce544582cd44d7c45ff7494a986
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2023-12-05 04:46:00 +0200

    tailwindcss added

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	runtime: 'edge',
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
