ffd0f530dd10f47c0dd3ac5b91d77204d982e3cb
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2023-12-06 18:12:19 +0200

    CSP settings for vercel web vitals

--- svelte.config.js
+++ svelte.config.js
@@ -19,8 +19,8 @@ const config = {
 		
 		csp: {
 			directives: {
-				'script-src': ['self', 'https://va.vercel-scripts.com'],
-				'connect-src': ['self', 'https://vitals.vercel-insights.com']
+				'script-src': ['self', 'https://*.vercel-scripts.com', 'https://*.vercel-insights.com'],
+				'connect-src': ['self', 'https://*.vercel-insights.com']
 			}
 		}
 	}
