af5e5d40064eab0dab39432bdc4d30022f3efac6
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2023-12-06 18:37:34 +0200

    CSP settings for vercel web vitals

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,11 @@
   
   import { Github, Youtube, Tractor, Sun, Moon } from "lucide-svelte";
   import { ModeWatcher, toggleMode } from "mode-watcher";
+
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <ModeWatcher />
