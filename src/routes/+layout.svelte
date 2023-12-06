59cd9c2a7cce6672b2bee6bc09c6d2dbf86fb1b1
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2023-12-06 14:10:49 +0200

    shancn components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,46 @@
 <script lang="ts">
   import "../app.pcss";
   
-  import { ModeWatcher } from "mode-watcher";
+  import { Button } from "$lib/components/ui/button";
+  import { Separator } from "$lib/components/ui/separator";
+  import { Slider } from "$lib/components/ui/slider";
+  import * as Table from "$lib/components/ui/table";
+  import * as Tabs from "$lib/components/ui/tabs";
+  
+  import { Github, Youtube, Tractor, Sun, Moon } from "lucide-svelte";
+  import { ModeWatcher, toggleMode } from "mode-watcher";
 </script>
 
 <ModeWatcher />
 
 <div class="container">
-  <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-    Layout
+  <h1 class="text-center mt-20 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    Pro Rata Type 
+    <Button on:click={toggleMode} variant="outline" size="icon">
+      <Sun
+        class="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      />
+      <Moon
+        class="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      />
+      <span class="sr-only">Toggle theme</span>
+    </Button>
+
+    <Button variant="outline" size="icon" href="https://github.com/andriytyurnikov/proratatype">
+      <Github
+        class="h-4 w-4 rotate-0 scale-100 transition-all"
+      />
+    </Button>
+    
+    <Button variant="outline" size="icon" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+      <Youtube
+        class="h-4 w-4 rotate-0 scale-100 transition-all"
+      />
+    </Button>
   </h1>
-  <p class="text-xl text-muted-foreground">
-    Common UI element between pages
+
+  <p class="mb-20 text-center text-xl text-muted-foreground">
+    Font sizing for truly responsive proportional typography
   </p>
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,103 @@
+<script lang="ts">
+  import "../app.pcss";
+
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import { Separator } from "$lib/components/ui/separator";
+  import { Slider } from "$lib/components/ui/slider";
+  import * as Table from "$lib/components/ui/table";
+  import * as Tabs from "$lib/components/ui/tabs";
+	import { Root } from "postcss";
+</script>
+
 <div class="container">
-  <p class="leading-7 [&:not(:first-child)]:mt-6">
-    The king, seeing how much happier his subjects were, realized the error of his
-    ways and repealed the joke tax.
-  </p>
+  <Tabs.Root value="google" class="w-[600px] mx-auto">
+    <Tabs.List class="grid w-full grid-cols-4">
+      <Tabs.Trigger value="google">Google Fonts</Tabs.Trigger>
+      <Tabs.Trigger value="system">System Fonts</Tabs.Trigger>
+      <Tabs.Trigger value="URL">URL</Tabs.Trigger>
+      <Tabs.Trigger value="upload">Upload</Tabs.Trigger>
+    </Tabs.List>
+
+    <Tabs.Content value="google">
+      <Card.Root>
+        <Card.Content>
+          Google fonts
+        </Card.Content>
+      </Card.Root>
+    </Tabs.Content>
+
+    <Tabs.Content value="system">
+      <Card.Root>
+        <Card.Content>
+          System fonts
+        </Card.Content>
+      </Card.Root>
+    </Tabs.Content>
+
+    <Tabs.Content value="URL">
+      <Card.Root>
+        <Card.Content>
+          Input font URL
+        </Card.Content>
+      </Card.Root>
+    </Tabs.Content>
+
+    <Tabs.Content value="upload">
+      <Card.Root>
+        <Card.Content>
+          Upload font
+        </Card.Content>
+      </Card.Root>
+    </Tabs.Content>
+
+  </Tabs.Root>
+
+  <Separator class="w-[400px] mx-auto" />
+
+  <Card.Root class="w-[600px] mx-auto">
+    <Card.Content>
+      <Slider class="my-4" value={[0.1]} min={0.0} max={1.0} step={0.01}></Slider>
+    </Card.Content>
+  </Card.Root>
+
+  <Separator class="w-[400px] mx-auto" />
+
+  <Card.Root class="w-[600px] mx-auto">
+    <Card.Content>
+      <Table.Root>
+        <Table.Caption>Proportional font-sizes per device</Table.Caption>
+
+        <Table.Header>
+          <Table.Row>
+            <Table.Head class="w-1/2">Device</Table.Head>
+            <Table.Head>Native font size, px</Table.Head>
+            <Table.Head>CSS font-size, px</Table.Head>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell class="font-medium">iPhone SE</Table.Cell>
+            <Table.Cell>16.00</Table.Cell>
+            <Table.Cell>17.01</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.Cell class="font-medium">iPhone 14</Table.Cell>
+            <Table.Cell>16.00</Table.Cell>
+            <Table.Cell>17.01</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.Cell class="font-medium">iPad Air</Table.Cell>
+            <Table.Cell>16.00</Table.Cell>
+            <Table.Cell>17.01</Table.Cell>
+          </Table.Row>
+
+        </Table.Body>
+      </Table.Root>
+    </Card.Content>
+  </Card.Root>  
+  
 </div>
